f62211ab13aa6b4a39e674f49c0f7344d7a56856
andrelo1 <andrelo.spam@gmail.com>
2022-02-01 15:47:55 +0300

    update version

--- src/version.h
+++ src/version.h
@@ -4,7 +4,7 @@ namespace Version
 {
 	inline constexpr std::size_t MAJOR = 2;
 	inline constexpr std::size_t MINOR = 0;
-	inline constexpr std::size_t PATCH = 3;
-	inline constexpr auto NAME = "2.0.3"sv;
+	inline constexpr std::size_t PATCH = 5;
+	inline constexpr auto NAME = "2.0.5"sv;
 	inline constexpr auto PROJECT = "gotobed"sv;
 }
