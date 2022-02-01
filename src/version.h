a763da3d77916f92a5330d02f94984aa2e27937a
andrelo1 <andrelo.spam@gmail.com>
2022-02-01 18:31:23 +0300

    change sv to const char

--- src/version.h
+++ src/version.h
@@ -5,6 +5,6 @@ namespace Version
 	inline constexpr std::size_t MAJOR = 2;
 	inline constexpr std::size_t MINOR = 0;
 	inline constexpr std::size_t PATCH = 5;
-	inline constexpr auto NAME = "2.0.5"sv;
-	inline constexpr auto PROJECT = "gotobed"sv;
+	inline constexpr auto NAME = "2.0.5";
+	inline constexpr auto PROJECT = "gotobed";
 }
