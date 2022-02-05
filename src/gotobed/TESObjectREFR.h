27007530e8f586c88d329b63800861657cf8a37b
andrelo1 <andrelo.spam@gmail.com>
2022-02-05 10:43:01 +0300

    Update TESObjectREFR.h

--- src/gotobed/TESObjectREFR.h
+++ src/gotobed/TESObjectREFR.h
@@ -7,7 +7,6 @@ namespace Gotobed
 	public:
 		bool	SetMarkerReserved_Hook(std::uint32_t a_marker, RE::Actor* a_actor, bool a_reserved, bool a_ignoreUsed);
 		bool	SetMarkerUsed_Hook(std::uint32_t a_marker, RE::Actor* a_actor, bool a_used, bool a_ignoreReserved);
-		//std::int32_t	GetFreeMarker(RE::Actor* a_actor, bool a_ignoreReserved);
 	};
 
 	namespace TESObjectREFRNS
