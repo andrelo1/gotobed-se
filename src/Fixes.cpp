45a8c0f567316765b9adac2b9963a50b94023165
andrelo1 <andrelo.spam@gmail.com>
2022-12-20 23:27:00 +0300

    refactoring

--- src/Fixes.cpp
+++ src/Fixes.cpp
@@ -8,24 +8,30 @@ namespace Gotobed::Fixes
 		class TESObjectREFR: public RE::TESObjectREFR
 		{
 		public:
-			bool SetMarkerReserved(std::uint32_t a_marker, RE::Actor* a_actor, bool a_reserved, bool a_ignoreUsed) {
-				auto middleHigh = a_actor->currentProcess->middleHigh;
-
-				if (middleHigh && middleHigh->unk2E4 != a_marker) {					
-					if (middleHigh->unk2E4 != -1) {
-						hook.call_orig(this, middleHigh->unk2E4, nullptr, false, false);
-					}
-					middleHigh->unk2E4 = a_marker;
-				}
-
-				return hook.call_orig(this, a_marker, a_actor, a_reserved, a_ignoreUsed);
-			}
-
-			static inline stl::HookData hook{&TESObjectREFR::SetMarkerReserved};
+			bool SetMarkerReserved(std::uint32_t a_marker, RE::Actor* a_actor, bool a_reserved, bool a_ignoreUsed);
 		};
 
+		namespace Hooks
+		{
+			stl::HookData SetMarkerReserved{&TESObjectREFR::SetMarkerReserved};
+		};
+
+		bool TESObjectREFR::SetMarkerReserved(std::uint32_t a_marker, RE::Actor* a_actor, bool a_reserved, bool a_ignoreUsed) {
+			auto middleHigh = a_actor->currentProcess->middleHigh;
+
+			if (middleHigh && middleHigh->unk2E4 != a_marker) {					
+				if (middleHigh->unk2E4 != -1) {
+					Hooks::SetMarkerReserved.call_orig(this, middleHigh->unk2E4, nullptr, false, false);
+				}
+				middleHigh->unk2E4 = a_marker;
+			}
+
+			return Hooks::SetMarkerReserved.call_orig(this, a_marker, a_actor, a_reserved, a_ignoreUsed);
+		}
+
+
 		void Install() {
-			stl::write_thunk(Offsets::BGSProcedureSitSleepExecState::ProcessActivate.address() + 0x02B4, TESObjectREFR::hook);
+			stl::write_thunk(Offsets::BGSProcedureSitSleepExecState::ProcessActivate.address() + 0x02B4, Hooks::SetMarkerReserved);
 			spdlog::info("Fixes: MultipleMarkersReservation installed");
 		}
 	}
